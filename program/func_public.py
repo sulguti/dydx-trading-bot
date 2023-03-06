be33a6b158acdf5dc8fe61561872b6f9002921c4
Sulguti <sulguti@outlook.com>
2023-03-06 15:44:26 +1100

    Third commit - up till Stage2

---
+++ program/func_public.py
@@ -0,0 +1,14 @@
+from constants import RESOLUTION
+from func_utils import get_ISO_times
+from pprint import pprint
+import pandas as pd
+import numpy as np
+import time
+
+# Get relevant time periods for ISO from and to
+ISO_TIMES = get_ISO_times()
+pprint(ISO_TIMES)
+
+# Construct market prices
+def construct_market_prices(client):
+      pass
--- program/main.py
+++ program/main.py
@@ -1,3 +1,33 @@
-name = input("What is your name")
-print(f"Hello {name}")
+from func_connection import connect_dydx
+from func_private import abort_all_positions
+from constants import ABORT_ALL_POSITIONS, FIND_COINTEGRATED
+from func_public import construct_market_prices
 
+if __name__ == "__main__":
+
+    # Connect to Client
+    try:
+        print("Connecting to Client...")
+        client = connect_dydx()
+    except Exception as e:
+        print("Error connecting to client: ", e)
+        exit(1)
+
+# Abort all open positions
+if ABORT_ALL_POSITIONS:
+    try:
+        print("Closing all positions...")
+        close_orders = abort_all_positions(client)
+    except Exception as e:
+        print("Error closing all positions: ", e)
+        exit(1)
+
+# Find Cointegrated Paris
+    if FIND_COINTEGRATED:
+        # Construct Market Prices
+        try:
+            print("Fetching Market Prices Please allow 3 minutes...")
+            df_market_prices = construct_market_prices(client)
+        except Exception as e:
+            print("Error constructing market prices: ", e)
+            exit(1)
